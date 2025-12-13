db817df3fda611585e782abafe44f2036da91418
mobina211 <kazemzadehmobina0@gmail.com>
2025-12-13 17:57:58 +0330

    add status dropDown

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 
-export const SHEET_ENDPOINT = "https://script.google.com/macros/s/AKfycbywch87vo4jk_HPRDuBjMzZNDOi9UiO8LF5MgIySKtyz-7kNldxrtYDCPCqqkOSYwd-aA/exec";
+export const SHEET_ENDPOINT = "https://script.google.com/macros/s/AKfycbxC63QhLCa9QRr3srgC6fCQZzh_AYNAQjjGbsdq55nKzdGXw61lyeptih4qpGawD-V8/exec";
