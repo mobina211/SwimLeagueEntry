58634d2863f8a5d5837bc4b446064bd42dc18249
mobina211 <kazemzadehmobina0@gmail.com>
2025-12-22 18:37:13 +0330

    normalize national code

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 
-export const SHEET_ENDPOINT = "https://script.google.com/macros/s/AKfycbzCXfLpEkDqb5NvJBLRJNopEPkFM-Ou0nIZ2DP-vb2wMkS82mMg8e-BLCXk4pNCTQgyxg/exec";
+export const SHEET_ENDPOINT = "https://script.google.com/macros/s/AKfycbyiMwSEI7RmUTRsXTlw_MX60Wb1LUhXr6VxEqQwV6RIQn-0g5LHdldD2EK1PRbnEQRQ5A/exec";
